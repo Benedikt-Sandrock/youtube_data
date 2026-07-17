bfb50caf5aa72d27a61c1220fe86911f69fe8fa4
Benedikt Sandrock <benedikt.sandrock@outlook.de>
2026-07-17 16:39:51 +0200

    3/20

---
+++ data/samples/russia/sample.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from youtube_code.config import SAMPLES, RAW
+from youtube_code.utils import load_json, save_json
+
+vdf =  load_json(RAW / "videos_total.json")
+
+vdf = [v for v in vdf if "Russland" in v["title"]]
+print(len(vdf))
+save_json("russia_vids.json", vdf)
